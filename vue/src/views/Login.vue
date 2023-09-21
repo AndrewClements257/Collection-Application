fe0148fedd5cb1f6bcad0b8604d14a3df8eab390
Andrew Clements <andrewclements257@gmail.com>
2023-09-20 23:53:14 -0400

    Started fixing the visuals on the login page.

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="login">
+    <h1 class="app-name">Collect-o 5000</h1>
     <form class="login-form" @submit.prevent="login">
-      <h1 >Please Sign In</h1>
+      <h1 class="login-title">Please Sign In</h1>
       <div role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
@@ -10,13 +11,13 @@
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input class="login-input" type="text" id="username" v-model="user.username" required autofocus />
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
+        <input class="login-input" type="password" id="password" v-model="user.password" required />
       </div>
-      <button type="submit">Sign in</button>
+      <button class="login-button" type="submit">Sign in</button>
       <p>
       <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
     </form>
@@ -77,6 +78,7 @@ label {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   width: 100%;
 }
 
@@ -87,11 +89,14 @@ label {
   max-width: 400px;
   width: 90%;
   background: rgba(255, 255, 255, 0.1); 
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.14);
   backdrop-filter: blur(10px); 
   border: 1px solid rgba(255, 255, 255, 0.2);
 }
 
 .login-title {
+  font-size: 2em;
+  font-weight: 100;
   text-align: center;
   margin-bottom: 32px;
 }
@@ -112,5 +117,21 @@ label {
   background-color: #ffc700;
   color: black;
   font-weight: bold;
+}
+
+.login-button:hover {
+  background-image: linear-gradient(to bottom right, #7f0cf2, dodgerblue);
+  cursor: pointer;
+  color: white;
+}
+
+.app-name {
+  font-size: 5em;
+  font-weight: 600;
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.14);
+  text-align: center;
+  margin-top: 32px;
+  margin-bottom: 64px;
+  color: white;
 }
 </style>
